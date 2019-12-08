09d0a150ddc8d97a99ca436bddc10b759ad05cdb
shakhzodzhan <imshakhzod@gmail.com>
2019-12-08 15:34:46 +0100

    FINAL VERSION WITHOUT FILTERS(((((

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -58,7 +58,7 @@ class TagsController < ApplicationController
   def destroy
     @tag.destroy
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+      format.html { redirect_to settings_path, notice: 'Tag was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
